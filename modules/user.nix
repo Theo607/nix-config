d65c0b8cf906cdb8db00ef56692ffd63771aa311
Mateusz Smuga <mateusz@smuga.pl>
2026-02-17 12:25:58 +0100

    small fix

--- modules/user.nix
+++ modules/user.nix
@@ -5,7 +5,6 @@
     isNormalUser = true;
     description = "Theo";
     extraGroups = [ "networkmanager" "wheel" ];
-    hashedPassword = "$6$Bafn6KUDGpPfXR8A$isnFoa3CJ0Lf7a1h.VXmueX.z1aPRUJg1EhQ3aQBZ4IJIktJk1bBS9lEMF2FoqYmDFckjjWU5q3doHLq0rXlA0";
   };
 
   # Optional: enable sudo without password
